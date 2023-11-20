9da41c20f71cc32d2abdd962430d9783aedeaa7c
Afzal Nomani <afzalnomani1@gmail.com>
2023-11-20 22:36:35 +0530

    importing components in better way

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,5 @@
 export { default as HomePage } from './HomePage';
 export { default as Pricing } from './Pricing';
-export { default as Product } from './Product';
+export {default as Products} from "./Products";
+export {default as PageNotFound} from "./PageNotFound";
+export {default as MaterialUI} from "./MaterialUI";
